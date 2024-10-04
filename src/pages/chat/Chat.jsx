7977013534ba107283313ee780acd5dce55d1690
Mahdi Rasooli <Mahdirasooli8029@gmail.com>
2024-10-04 18:06:43 +0330

    complete UI for ChatBox

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import  './Chat.css'
+import LeftSideBar from '../../components/leftSideBar/LeftSideBar'
+import ChatBox from '../../components/chatBox/ChatBox'
+import RightSideBar from '../../components/rightSideBar/RightSideBar'
 
 const Chat = () => {
   return (
-    <div>
-      chat
+    <div className='chat'>
+      <div className="chat-container">
+        <LeftSideBar />
+        <ChatBox />
+        <RightSideBar />
+      </div>
     </div>
   )
 }
