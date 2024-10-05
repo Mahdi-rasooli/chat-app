4bd45c3ee3b3737d3fd73f808a862d9c76df8364
Mahdi Rasooli <Mahdirasooli8029@gmail.com>
2024-10-05 14:42:25 +0330

    Complete UI for rightside

--- src/components/rightSideBar/RightSideBar.jsx
+++ src/components/rightSideBar/RightSideBar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './RightSideBar.css'
+import assets from '../../assets/assets'
 
 const RightSideBar = () => {
   return (
-    <div></div>
+    <div className='rs'>
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>RIchard stanford<img src={assets.green_dot} alt=""  className='dot'/></h3>
+        <p>Hey i am using messagner</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic1} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>
+    </div>
   )
 }
 
