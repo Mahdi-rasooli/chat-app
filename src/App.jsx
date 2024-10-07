75ddb47a33d2875bb6d33ec2aba6893b4c706121
Mahdi Rasooli <Mahdirasooli8029@gmail.com>
2024-10-07 23:44:44 +0330

    Create navigate for logout and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
-  BrowserRouter as Router,
   Route,
-  Routes
+  Routes,
 } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 import Login from '../src/pages/login/Login'
 import Profile from '../src/pages/profile-update/Profile'
 import Chat from '../src/pages/chat/Chat'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { auth } from "./config/fireBase";
+import { onAuthStateChanged } from "firebase/auth";
 
 
 function App() {
 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        navigate('/chat')
+      } else {
+        navigate('/')
+      }
+    })
+  }, [])
+
   return (
     <>
-      <Router>
-        <div>
-          <ToastContainer/>
-          <Routes>
-            <Route path='/' element={<Login/>}/>
-            <Route path='/profile' element={<Profile/>}/>
-            <Route path='/chat' element={<Chat/>}/>
-          </Routes>
-        </div>
-      </Router>
+      <div>
+        <ToastContainer />
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/chat' element={<Chat />} />
+        </Routes>
+      </div>
     </>
   )
 }
