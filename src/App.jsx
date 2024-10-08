60a5138fb63246e707621854f3964cd09e6d1aa9
Mahdi Rasooli <Mahdirasooli8029@gmail.com>
2024-10-08 12:55:02 +0330

    Create and set up appContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import {
   Route,
   Routes,
@@ -11,16 +11,19 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { auth } from "./config/fireBase";
 import { onAuthStateChanged } from "firebase/auth";
+import { AppContext } from "./context/AppContext";
 
 
 function App() {
 
   const navigate = useNavigate();
+  const {loadUserData} = useContext(AppContext)
 
   useEffect(() => {
     onAuthStateChanged(auth, async (user) => {
       if (user) {
         navigate('/chat')
+        await loadUserData(user.uid)
       } else {
         navigate('/')
       }
